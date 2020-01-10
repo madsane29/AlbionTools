095e72b46f613e97fc486f8047a12dbf51929e54
MadSane <madsane29@gmail.com>
2020-01-10 22:26:17 +0100

    Start of the project

---
+++ src/main/java/com/albiontools/AlbionToolsApplication.java
@@ -0,0 +1,13 @@
+package com.albiontools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlbionToolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlbionToolsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/albiontools/AlbionToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.albiontools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlbionToolsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
