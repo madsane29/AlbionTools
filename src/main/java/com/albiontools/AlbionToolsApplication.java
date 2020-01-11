57a3b321bbb48ed54753999df86c9827152822b5
MadSane <madsane29@gmail.com>
2020-01-11 02:31:12 +0100

    HTML pages, i18n - english and hungarian

--- src/main/java/com/albiontools/AlbionToolsApplication.java
+++ src/main/java/com/albiontools/AlbionToolsApplication.java
@@ -2,9 +2,11 @@ package com.albiontools;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
 
 @SpringBootApplication
-public class AlbionToolsApplication {
+public class AlbionToolsApplication extends WebMvcAutoConfiguration {
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AlbionToolsApplication.class, args);
--- src/main/java/com/albiontools/controller/UserController.java
+++ src/main/java/com/albiontools/controller/UserController.java
@@ -8,7 +8,35 @@ public class UserController {
 	
 	@GetMapping("/")
 	public String getHome() {
-		return "home";
+		return "index";
 	}
 
+	@GetMapping("/trading")
+	public String getTradingPage() {
+		return "trading";
+	}
+	
+	@GetMapping("/change-password")
+	public String getChangePasswordPage() {
+		return "relatedToUserAccounts/change-password";
+	}
+	
+	@GetMapping("/verification")
+	public String a() {
+		return "relatedToUserAccounts/verification";
+	}
+	
+	@GetMapping("/registration")
+	public String b() {
+		return "relatedToUserAccounts/registration";
+	}
+	
+	@GetMapping("/login")
+	public String c() {
+		return "relatedToUserAccounts/login";
+	}
+	
+	
+	
+	
 }
